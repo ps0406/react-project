9e273de8d878b769898f8669a01d218be3727765
Yap Pei Sin <peisin.yap@growthops.asia>
2023-03-07 17:34:52 +0800

    cleanup: remove unused code

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -4,20 +4,14 @@ import Card from "../UI/Card";
 import { useState } from 'react'
 
 const ExpenseItem = (props) => {
-  const [title, setTitle] = useState(props.title); 
-
-  const clickHandler = () => {
-    setTitle("Updated"); 
-    console.log(title)
-  }
+  
   return (
     <Card className="expense-item">
       <ExpenseDate date = {props.date}></ExpenseDate>
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{props.propstitle}</h2>
         <div className="expense-item__price">{props.amount}</div>
-      </div>
-      <button onClick={clickHandler}>Change Title</button>
+      </div> 
     </Card>
   );
 }
