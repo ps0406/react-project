96e4b5898b8456c2913832b5d9837cdc3acee51e
Yap Pei Sin <peisin.yap@growthops.asia>
2023-03-07 17:35:11 +0800

    feat: add expense filter

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,22 +1,32 @@
+import React, { useState } from 'react'; 
 import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
 import "./Expenses.css";
+import ExpensesFilter from "./ExpensesFilter";
 
 const Expense = (props) => {
+  const [filteredYear, setFilteredYear] =  useState("2020");
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
+
   return (
-    <Card className="expenses">
-      {props.expense.map((item, index) => {
-        return (
-          <ExpenseItem
-            key={index}
-            title={item.title}
-            amount={item.amount}
-            date={item.date}
-          ></ExpenseItem>
-        );
-      })}
-    </Card>
+    <div>
+      <Card className="expenses">
+        <ExpensesFilter selected={filteredYear} onFilterChange={filterChangeHandler}></ExpensesFilter>
+        {props.expense.map((item, index) => {
+          return (
+            <ExpenseItem
+              key={index}
+              title={item.title}
+              amount={item.amount}
+              date={item.date}
+            ></ExpenseItem>
+          );
+        })}
+      </Card>
+    </div>
   );
-}
+};
 
 export default Expense;
